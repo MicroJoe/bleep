2c2f9bd4781e297c98f34fbe47cf5144c30134e1
Agathe Porte <microjoe@microjoe.org>
2022-03-07 15:16:07 +0100

    use new source for macosx sounds

--- macos-big-sur-builtins.sh
+++ macos-big-sur-builtins.sh
@@ -6,24 +6,44 @@
 # This script is here for obvious copyright reasons:
 # distributing such files may be a copyright infringement.
 
-if ! command -v ffmpeg
+ZIPFILE=BigSur-Sounds-All.zip
+URL=https://bigsur-sounds.itsnoahevans.co.uk/$ZIPFILE
+DOWNLOAD=true
+
+if ! command -v ffmpeg > /dev/null
 then
     echo "You need to install ffmpeg first."
     exit 1
 fi
 
-rm -f macOS-Big-Sur-Sounds.zip
-rm -Rf builtins/macos/
-wget https://appletldposts.com/wp-content/uploads/2020/07/macOS-Big-Sur-Sounds.zip
-unzip -LL macOS-Big-Sur-Sounds.zip
-rm macOS-Big-Sur-Sounds.zip
-mkdir -p builtins
-mv "macos big sur sounds/" builtins/macos/
+
+if "$DOWNLOAD"
+then
+	rm -r "__macos"
+	rm -r "sounds"
+	rm "$ZIPFILE"
+	rm -R builtins/macos/
+
+	wget "$URL"
+	unzip -LL "$ZIPFILE"
+
+	rm "$ZIPFILE"
+	mkdir -p builtins
+	mv "sounds/" builtins/macos/
+fi
 
 # rodio does not support AIFF playback.
 # files are converted to FLAC which is lossless and compressed
-for f in builtins/macos/*.aiff
+for ext in 'aiff' 'aif' 'caf' 'm4r'
 do
-    ffmpeg -y -i $f builtins/macos/$(basename $f .aiff).flac
-    rm $f
+	for f in $(find builtins/macos/ -type f -name "*.$ext")
+	do
+	    ffmpeg -y -i $f builtins/macos/$(basename $f ".$ext").flac
+	    rm $f
+	done
+done
+
+for d in $(find builtins/macos/ -mindepth 1 -type d)
+do
+	rm -r "$d"
 done
